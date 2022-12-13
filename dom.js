0f746a953aee06b6e427e90c257c02173b45aec3
NPB98 <nishibiswasroy@gmail.com>
2022-12-13 14:13:51 +0530

    JS file

---
+++ dom.js
@@ -0,0 +1,24 @@
+//EXAMINE THE DOCUMENT OBJECT
+//console.dir(document);
+//console.log(document.all);
+//console.log(document.all[10]);
+//console.log(domain);
+console.log(URL);
+console.log(document.title);
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.forms);
+console.log(document.forms[0]);
+console.log(document.images);
+console.log(document.links);
+console.log(document.getElementById('header-title'));
+var headerTitle=document.getElementById('header-title');
+headerTitle.textContent="Hello";
+headerTitle.innerText="Goodbye";
+headerTitle.innerHTML='<h3>Hello</h3>';
+headerTitle.style.borderBottom='solid 3px #000';
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000';
+var border=document.getElementById('main');
+border.style.color='green';
