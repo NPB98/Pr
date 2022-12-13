8934c9cad573e7ef94465e77cba89bac077c1e7f
NPB98 <nishibiswasroy@gmail.com>
2022-12-13 19:58:13 +0530

    Task 6 Part 2

--- dom.js
+++ dom.js
@@ -49,19 +49,35 @@
 // {
 //     li[i].style.backgroundColor='#f4f4f4';
 // }
-var header=document.querySelector('#main-header');
-header.style.borderBottom='solid 4px #ccc';
-var input=document.querySelector('input');
-input.value='Hello World';
-var submit=document.querySelector("input[type='submit']");
-submit.value='SEND';
-var item=document.querySelector('.list-group-item');
-item.style.color='red';
-var lastItem=document.querySelector('.list-group-item:last-child');
-lastItem.style.color='blue';
-var secondItem=document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.color='coral';
-secondItem.style.backgroundColor='green';
-var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.visibility='hidden';
+// var header=document.querySelector('#main-header');
+// header.style.borderBottom='solid 4px #ccc';
+// var input=document.querySelector('input');
+// input.value='Hello World';
+// var submit=document.querySelector("input[type='submit']");
+// submit.value='SEND';
+// var item=document.querySelector('.list-group-item');
+// item.style.color='red';
+// var lastItem=document.querySelector('.list-group-item:last-child');
+// lastItem.style.color='blue';
+// var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.color='coral';
+// secondItem.style.backgroundColor='green';
+// var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.visibility='hidden';
 
+var titles=document.querySelectorAll('.title');
+console.log(titles);
+titles[0].textContent="Hello";
+var odd=document.querySelectorAll('li:nth-child(odd)');
+var even=document.querySelectorAll('li:nth-child(even)');
+for(var i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor='green';
+    even[i].style.backgroundColor='#ccc';
+}
+var items=document.querySelectorAll('.items');
+//for(var j=0;j<items.length;j++)
+//{
+  //  if(j==1)
+    //    items[j].style.color='green';
+//}
