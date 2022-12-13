b53dbfe6b4d43c30f4e522137d451477b4bcd117
NPB98 <nishibiswasroy@gmail.com>
2022-12-13 19:17:59 +0530

    Task 5

--- dom.js
+++ dom.js
@@ -22,22 +22,31 @@
 // header.style.borderBottom='solid 3px #000';
 // var border=document.getElementById('main');
 // border.style.color='green';
-var items=document.getElementsByClassName("list-group-item");
-console.log(items);
-console.log(items[1]);
-items[1].textContent="Hello 2";
-items[1].style.fontWeight ='bold';
-items[1].style.backgroundColor ='yellow';
-//Error
-//items.style.backgroundColor='yellow';
-for(var i=0;i<items.length;i++)
+// var items=document.getElementsByClassName("list-group-item");
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent="Hello 2";
+// items[1].style.fontWeight ='bold';
+// items[1].style.backgroundColor ='yellow';
+// //Error
+// //items.style.backgroundColor='yellow';
+// for(var i=0;i<items.length;i++)
+// {
+//     items[i].style.backgroundColor='#f4f4f4';
+// }
+// items[2].style.backgroundColor='green';
+// for(var i=0;i<items.length;i++)
+// {
+//     items[i].style.fontWeight='bold';
+// }
+var li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent="Hello";
+li[1].style.fontWeight='bold';
+li[1].style.backgroundColor='yellow';
+for(var i=0;i<li.length;i++)
 {
-    items[i].style.backgroundColor='#f4f4f4';
+    li[i].style.backgroundColor='#f4f4f4';
 }
-items[2].style.backgroundColor='green';
-for(var i=0;i<items.length;i++)
-{
-    items[i].style.fontWeight='bold';
-}
-
 
