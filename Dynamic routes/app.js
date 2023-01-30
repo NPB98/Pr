beaf2d83895810c493caebe7ca3c9bcf4001ffeb
NPB98 <nishibiswasroy@gmail.com>
2023-01-30 12:05:15 +0530

    Sequelize Express Js Task 17

--- Dynamic routes/app.js
+++ Dynamic routes/app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 
 const app = express();
-const db=require('./util/database');
+const sequelize=require('./util/database');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -20,7 +20,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-
 app.use(errorController.get404);
 
-app.listen(4000);
+sequelize.sync()
+.then((result)=>{
+    //console.log(result);
+    app.listen(4000);
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+
--- Dynamic routes/controllers/shop.js
+++ Dynamic routes/controllers/shop.js
@@ -2,24 +2,23 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows])=>{
+  Product.findAll().then(products=>{
     res.render('shop/product-list', {
-      prods: rows,
+      prods: products,
       pageTitle: 'All Products',
       path: '/products'
-  });
     })
-    .catch((err)=>console.log(err));
+  })
+  .catch((err)=>console.log(err))
 };
 
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-  .then(([product])=>{
+  Product.findByPk(prodId)
+  .then((product)=>{
     res.render('shop/product-detail', {
-      product: product[0],
+      product: product,
       pageTitle: product.title,
       path: '/products'
   })
@@ -28,15 +27,14 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows,fieldData])=>{
+  Product.findAll().then(products=>{
     res.render('shop/product-list', {
-      prods: rows,
+      prods: products,
       pageTitle: 'All Products',
       path: '/products'
     })
-  .catch((err)=>console.log(err));
-  });
+  })
+  .catch((err)=>console.log(err))
 };
 
 exports.getCart = (req, res, next) => {
@@ -60,7 +58,7 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId).then(([rows,fieldData])=>{
+  Product.findByPk(prodId).then(([rows,fieldData])=>{
     Cart.addProduct(prodId, product.price);
   })
   .catch((err)=>console.log(err));
@@ -69,7 +67,7 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId).then(([rows,fieldData])=>{
+  Product.findByPk(prodId).then(([rows,fieldData])=>{
     Cart.deleteProduct(prodId, fieldData[0].price);
     res.redirect('/cart');
   }) 
