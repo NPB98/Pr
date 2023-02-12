5cd6fa0acaf8168663809f8af69979ca0b566722
NPB98 <91026118+NPB98@users.noreply.github.com>
2023-02-12 17:05:58 +0530

    Delete Login Page directory

--- Login Page/routes/login.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-const userController = require('../controllers/login');
-
-router.post('/user/signup',userController.addUser);
-
-module.exports = router;    
